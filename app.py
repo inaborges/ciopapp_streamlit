fe59e820cbe4d5fc34d9d546a4861b3eeae9ecd7
inaborges <marina.borges.adm@gmail.com>
2020-06-25 18:54:01 -0300

    Fixed same key atribbute on buttons.

--- app.py
+++ app.py
@@ -16,17 +16,17 @@ st.subheader('2014')
 data2014: object = pd.read_csv('2014-improved-property-sales.csv')
 st.write(data2014.head())
 
-total_sales_14 = data2014['Sale Price'].sum().round()
+total_sales_2014 = data2014['Sale Price'].sum().round()
 total_cm_2014 = data2014['Total Mgt. Expenses'].sum().round()
-year_r_2014 = total_sales_14 - total_cm_2014
+year_r_2014 = total_sales_2014 - total_cm_2014
 
-if st.button('Total Sales'):
-    st.write(total_sales_14)
+if st.button('Total Sales 2014'):
+    st.write(total_sales_2014)
 
-if st.button('Total Management Expenses'):
+if st.button('Total Management Expenses 2014'):
     st.write(total_cm_2014)
 
-if st.button('Years Result'):
+if st.button('2014 Years Result'):
     st.write(year_r_2014)
 
 #Data Sample and buttons 2015
@@ -35,9 +35,20 @@ st.subheader('2015')
 data2015: object = pd.read_csv('2015-improved-property-sales.csv')
 st.write(data2015.head())
 
-total_sales_15 = data2015['Sale Price'].sum().round()
+total_sales_2015 = data2015['Sale Price'].sum().round()
 total_cm_2015 = data2015['Total Mgt. Expenses'].sum().round()
+year_r_2015 = total_sales_2015 - total_cm_2015
 
+if st.button('Total Sales 2015'):
+    st.write(total_sales_2015)
+
+if st.button('Total Management Expenses 2015'):
+    st.write(total_cm_2015)
+
+if st.button('2015 Years Result'):
+    st.write(year_r_2015)
+
+#Data Sample and buttons 2016
 st.subheader('2016')
 
 data2016: object = pd.read_csv('2016-improved-property-sales.csv')
